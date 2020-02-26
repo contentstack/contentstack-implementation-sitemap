04f21ce3cb3b7e5d26f4d789dfeebdd971616fef
abhishek305 <abhishekshaji1994@gmail.com>
2020-02-26 16:39:39 +0530

    first committ

---
+++ utils.js
@@ -0,0 +1,52 @@
+
+const axios = require('axios');
+const fs = require('fs');
+const configVars = require('./config');
+
+function createSitemap(mapping) {
+  const stream = fs.createWriteStream('sitemap.xml');
+  stream.write('<?xml version="1.0" encoding="utf-8" standalone="yes" ?>');
+  stream.write(
+    '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+  );
+  mapping.map((index) => {
+    stream.write('\n  <url>\n');
+    stream.write(`    <loc>${configVars.rootPath.path + index.urls}</loc>\n`);
+    stream.write(`    <lastmod>${index.lastmod}</lastmod>\n`);
+    stream.write(`    <changefreq>${index.changfreq}</changefreq>\n`);
+    stream.write(`    <priority>${index.priority}</priority>\n`);
+    stream.write('  </url>');
+  });
+  stream.write('\n</urlset>');
+  stream.end();
+}
+
+
+// Write SyncFile with SyncToken
+function syncWriteFunction(syncTokenVar) {
+  fs.writeFileSync('./syncToken.txt', syncTokenVar, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Mapped file created');
+    }
+  });
+}
+
+// Axios wrapper
+function getData(url) {
+  const headerData = {
+    headers: {
+      api_key: configVars.apiKey,
+      access_token: configVars.accessToken,
+    },
+  };
+  return axios.get(url, headerData);
+}
+
+// exports library
+module.exports = {
+  getData,
+  syncWriteFunction,
+  createSitemap,
+};
