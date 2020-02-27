e509e5cf965f599111c03b285160f39ec2e662e1
abhishek305 <abhishekshaji1994@gmail.com>
2020-02-27 20:05:03 +0530

    changes

--- config.js
+++ config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  port: 4000,
-  apiKey: 'bltff856f3f034d89c7',
-  accessToken: 'cs037658fd3f355e61abdec712',
-  env: 'development',
+  port: '', //mention port
+  apiKey: '',// apikey
+  accessToken: '', //accessToken
+  env: '', // environment
   headerSection: {
-    headerContentTypeId: 'express_header',
-    headerEnrtyId: 'blt8085f8a971ce28a6',
+    headerContentTypeId: '', 
+    headerEnrtyId: '',
   },
   footerSection: {
-    footerContentTypeId: 'express_footer',
-    footerEnrtyId: 'bltf06662fffeb4e57a',
+    footerContentTypeId: '',
+    footerEnrtyId: '',
   },
   homeSection: {
-    homeContentTypeId: 'home_express',
-    homeEnrtyId: 'blta517cc7eaf012da7',
+    homeContentTypeId: '',
+    homeEnrtyId: '',
   },
   expressBlogSection: {
-    blogContentTypeId: 'express_blog',
+    blogContentTypeId: '',
   },
   unTrackedUrls: {
-    unTrackedUrlsContentTypeId: 'untracked_urls',
-    unTrackedUrlsEntryId: 'bltf06179782511ce4b',
+    unTrackedUrlsContentTypeId: '',
+    unTrackedUrlsEntryId: '',
   },
   rootPath: {
-    path: 'http://localhost:4000',
+    path: '', // your website's url (eg: www.yoursitename.com)
   },
 };
